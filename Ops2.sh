90a7c25a3a2ffca4e22d5df553a65ad63a988f90
Ray <ray@ray.blanton1217@gmail.com>
2024-11-26 19:34:09 -0600

    ops2.sh

--- Ops2.sh
+++ Ops2.sh
@@ -1,5 +1,32 @@
-# #!bin/bash
-# Declare a variable with as your name
-# Print to the screen Welcome to the Class Name(variable)
-name="Storm"
-echo "Welcome to the class $name"
+#!/bin/bash
+# Install whois on your kali
+
+# Add to your bash script a sixth option that calls a function to:
+function runcommands() {
+    # Take a user input string. Presumably the string is a domain name such as Google.com.
+    echo "enter a domian name"
+    read name
+    # Run whois against a user input string.
+    {
+     whois $name  
+    # Run dig against the user input string.
+            dig $name 
+    # Run host against the user input string.
+            host $name
+    # Run nslookup against the user input string.
+        nslookup $name
+
+
+
+
+    } > "file.txt"
+    
+
+
+
+    
+}
+runcommands 
+# Output the results to a single .txt file and open it with your favorite text editor.
+
+# For this challenge you must use at least one variable and one function.
